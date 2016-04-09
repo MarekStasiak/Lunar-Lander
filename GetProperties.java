c99354e4be32b296b104bccdc7875f58818c2ee4
MarekStasiak <new.marek.stasiak@gmail.com>
2016-04-09 10:14:12 +0200

    Create GetProperties.java

---
+++ GetProperties.java
@@ -0,0 +1,10 @@
+import java.io.IOException;
+ 
+public class GetProperties{
+	 
+	public static void main(String[] args) throws IOException {
+		GetPropertyValues properties = new GetPropertyValues();
+		properties.getPropValues();
+	}
+}
+
